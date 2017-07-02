4f17bcf22a1a0410749f485f3af1cafbd2b9f78e
Bryan Harris <bharris8@nd.edu>
2017-07-02 17:31:44 -0400

    time progression

--- tailsim.py
+++ tailsim.py
@@ -12,74 +12,252 @@ class Time:
     second = 1e6
     hour = 3.6e9
 
-class SimError(Exception):
-    """A basic simulation error"""
+class SimulationError(Exception):
+    """A logical error in the simulation."""
     def __init__(self, time, message=None):
+        super(SimulationError, self).__init__()
         self.time = time
         self.message = message
 
     def __str__(self):
-        return "Simulation Error at {}: {}".format(self.time, self.message)
+        return "Simulation Error at time {}: {}".format(self.time, self.message)
     def __repr__(self):
-        return "{}({},{})".format(self.__class__.__name__, self.time, self.message)
+        return "{}({},{})".format(self.__class__.__name__, repr(self.time), repr(self.message))
 
-class SimEvent:
+class Event(object):
     """
-    A SimEvent is any action that changes the state of the simulation.
+    An Event is any action that changes the state of the simulation.
     It has a scheduled simulation time (when the event should occur)
-    and an action (a lambda expression) that will be called with the
-    current simulation time when the event will be applied.
+    and an action (a lambda expression) that is called with the
+    current time when the event is applied.
     """
-    def __init__(self, scheduled_time, action):
-        self._time = scheduled_time
-        self._action = action
+    def __init__(self, actor, scheduled_time, action):
+        self.actor = actor
+        self.time = scheduled_time
+        self.action = action
 
     def __call__(self, time):
-        """Perform the action of this event"""
-        self._action(time)
+        """
+        Perform the action of this Event, applying it to the state of the
+        simulation.  The current simulation time is passed as the
+        argument.
+        """
+        self.action(time)
 
     def __repr__(self):
-        return "{}({}, {})".format(self.__class__.__name__, self._time, self._action)
+        return "{}({}, actor={}, action={})".format(self.__class__.__name__, self.time, self.actor, self.action)
 
-class SimActor:
+class Actor(object):
     """
-    A SimActor is anything in the simulation that can create
-    SimEvents.
-    """
-    def __init__(self, sim):
-        self._sim = sim
-
-class Simulation:
-    """
-    A Simulation maintains a simulation timeline, has a collection of
-    SimActors that submit SimEvents, and performs the SimEvents
-    submitted.
+    An Actor influences a Simulation by changing its state through the
+    generation (and application) of Events.  A Simulation first
+    informs an Actor that it is bound to the Simulation and that it is
+    ready to receive Events.  The Actor then schedules Events with the
+    Simulation, which applies them at the appropriate simulation time.
+    An Actor may request that an Event be canceled if it has not yet
+    been applied.
     """
     def __init__(self):
-        self._actors = []
-        self._events = []
+        self._simulation = None
+
+    @property
+    def simulation(self):
+        """The Simulation this Actor is bound to"""
+        if self._simulation is None:
+            raise SimulationError(None, "{} is not registered with any Simulation: {}".format(self.__class__.__name__, self))
+        return self._simulation
+
+    @simulation.setter
+    def simulation(self, simulation):
+        self.bind_to(simulation)
+
+    def bind_to(self, simulation):
+        """
+        Binds this Actor to the given Simulation.  Any subsequent Events
+        generated may be scheduled with the Simulation specified.
+        """
+        if not isinstance(simulation, Simulation):
+            raise ValueError("{} must be a Simulation".format(simulation))
+        self._simulation = simulation
+
+    def schedule_event(self, time, action):
+        """
+        Schedules an Event with the Simulation this Actor is bound to.
+        The return value is the argument to pass to cancel_event() to
+        cancel this action.
+        """
+        event = Event(self, time, action)
+        return self.simulation.schedule(event)
+
+    def cancel_event(self, handle):
+        """
+        Request that the Simulation cancel an event specified by the
+        argument.  The argument must be something returned by
+        schedule_event().
+        """
+        self.simulation.cancel(handle)
+
+    def commit_to(self, future_time):
+        """
+        Gets a commitment from this Actor before advancing the simulation
+        time.  This is a negotiation between the Actor and Simulation,
+        and the Actor is not required to commit to as much time as
+        requested.  The Simulation calls this function before
+        advancing the simulation time to get the Actor's promise that
+        it will schedule no more events before the given time.  This
+        is a "last call" for the Actor to do so, and is useful for
+        stochastic Actors to insert random events.  The returned value
+        is the time this Actor is willing to commit to, which may be
+        before the requested future_time, but may not be in the past
+        (before the current simulation time).  An Actor may schedule
+        new events at the current time (now), but notice that never
+        committing to a time in the future prevents time from
+        progressing, and the simulation will stop.  A Simulation
+        should never request a commitment beyond any Actor's next
+        event.  This base class implementation always commits the
+        Actor to whatever time is requested.  A subclass (such as a
+        stochastic Actor) must override this if it is interested in
+        inserting new (e.g. random) events or negotiating time
+        progression.
+        """
+        return future_time
+
+    def start(self):
+        """
+        Called by the Simulation to notify the Actor that the Simulation
+        is about to start.  This allows the Actor to schedule its
+        initial Events.  (A subclass should override if interested.)
+        """
+        pass
+
+class Simulation(object):
+    """
+    A Simulation maintains a simulation timeline, has a collection of
+    Actors that submit Events, and applies the Events submitted at the
+    appropriate simulation time.
+    """
+    def __init__(self):
+        self.now = 0
+        self.actors = []
+        self._schedule = []
+        self._canceled_events = []
+        self._past_events = []
+        self._commit_of = {}  # times Actors have committed to
+        self._commit_time = self.now  # latest unanimous commitment
 
     def add_actor(self, actor):
-        """Adds an actor to this simulation"""
-        if not isinstance(actor, SimActor):
-            raise ValueError("Must be a simulation actor (SimActor): {}".format(actor))
-        self._actors.append(actor)
+        """Adds an actor to this Simulation"""
+        if not isinstance(actor, Actor):
+            raise ValueError("Must be a simulation actor (Actor): {}".format(actor))
+        self.actors.append(actor)
+        actor.bind_to(self)
+        self._commit_of[actor] = self.now
 
-    def register(self, event):
-        """Registers a SimEvent in the simulation"""
+    def schedule(self, event):
+        """
+        Schedules an Event to be applied to the Simulation.  Returns the
+        argument to pass to cancel() should the Actor decide to cancel
+        this Event.
+        """
+        if event.time < self.now:
+            raise SimulationError(self.now, "Can't schedule Event in the past: {}".format(event))
+        if event.time < self._commit_of[event.actor]:
+            raise SimulationError(self.now, "{} violates {}'s commitment of time {}".format(event, event.actor, self._commit_of[event.actor]))
+        self._schedule.append(event)
+        # Here, we simply return the event itself, but this should be
+        # considered an implementation detail.  What the return value
+        # really is should be opaque to the Actor, but it must
+        # identify the event sufficiently to cancel() it.
+        return event
 
-        self._events
-        
+    def cancel(self, handle):
+        """Attempt to cancel a scheduled Event"""
+        # In this implementation, schedule() returns the event itself,
+        # so we just have to try to remove it from the schedule.
+        try:
+            self._schedule.remove(handle)
+        except ValueError:
+            raise SimulationError(self.now, "Cannot cancel event {}".format(handle))
+        self._canceled_events.append(handle)
+        self.log("Canceled {}".format(handle))
 
-# class ScheduledEvent(SimEvent):
-#     def __init__(self, time, action):
-#         super(ScheduledEvent, self).__init__(time, action)
-        
-# class RandomEvent(SimEvent):
-#     def __init__(self, time, action):
-#         super(RandomEvent, self).__init__(time, action)
+    def cancel_all_from(self, actor):
+        """
+        Cancel all scheduled events from the given Actor.  Returns the
+        number of events canceled.
+        """
+        num_canceled = 0
+        new_schedule = []
+        for event in self._schedule:
+            if event.actor == actor:
+                self._canceled_events.append(event)
+                self.log("Canceled {}".format(event))
+                num_canceled += 1
+            else:
+                new_schedule.append(event)
+        self._schedule = new_schedule
+        return num_canceled
 
-class WorkUnit:
+    def log(self, msg):
+        """Logs the given message"""
+        print(self.now, msg, file=sys.stderr)
+
+    def take_next(self):
+        """
+        Gets a commitment to an event, removes it from the schedule, and
+        yields it.
+        """
+        while True:
+            # First event may be within commitment
+            try:
+                self._schedule = sorted(self._schedule, key=lambda e: e.time)
+                next_event = self._schedule[0]
+                if self._commit_time >= next_event.time:
+                    yield self._schedule.pop(0)
+                    continue
+            except IndexError:
+                break
+
+            # Try to extend commitment
+            for actor in self.actors:
+                new_commit = actor.commit_to(next_event.time)
+                if new_commit < self._commit_of[actor]:
+                    raise SimulationError(self.now, "{} cannot lower commitment from {} to {}".format(actor, self._commit_of[actor], new_commit))
+                self._commit_of[actor] = new_commit
+            self._commit_time = min(self._commit_of.values())
+
+            # Stop if no consensus to next event
+            try:
+                self._schedule = sorted(self._schedule, key=lambda e: e.time)
+                if self._commit_time < self._schedule[0].time:
+                    self.log("Can't get commitment to next event: {}".format(self._schedule[0]))
+                    return
+                else:
+                    yield self._schedule.pop(0)
+            except IndexError:
+                break
+
+        # Finished
+        self.log("No events left")
+        return
+
+    def start(self):
+        """
+        Starts the Simulation.  The Simulation stops when there are no
+        remaining scheduled Events.
+        """
+        self.log("{} has {} actors and {} scheduled events".format(self.__class__.__name__, len(self.actors), len(self._schedule)))
+        if len(self.actors) > 0:
+            self.log("Sending start notifications to Actors")
+            for actor in self.actors:
+                actor.start()
+        self.log("Starting at time {} with {} scheduled events".format(self.now, len(self._schedule)))
+        for event in self.take_next():
+            self.now = event.time
+            event(self.now)
+        self.log("Done")
+
+class WorkUnit(object):
     """
     A model of a single unit of work.  Not all WorkUnits are created
     equally, and some may require more running time than others (even
@@ -105,7 +283,7 @@ class WorkUnit:
     def process(self, time):
         """Mark this WorkUnit as processed"""
         if self._processed != None:
-            raise SimError(time, "{} already processed".format(self))
+            raise SimulationError(time, "{} already processed".format(self))
         if time is None:
             raise ValueError("No processing time given")
         self._processed = time
@@ -113,7 +291,7 @@ class WorkUnit:
     def __repr__(self):
         return "{}({},{})".format(self.__class__.__name__, self._scale, self._processed)
 
-class TaskRun:
+class TaskRun(object):
     """
     A TaskRun is a record of an attempt to run a Task on a Host.  It
     is a simple state machine that enforces state transitions and
@@ -188,7 +366,7 @@ class TaskRun:
         self._finish = time
 
     def invalid_transition_to(self, to_state, time):
-        raise SimError(time, "Invalid state transition: {} to {}".format(TaskRun.state_str(self._state), TaskRun.state_str(to_state)))
+        raise SimulationError(time, "Invalid state transition: {} to {}".format(TaskRun.state_str(self._state), TaskRun.state_str(to_state)))
 
     def __repr__(self):
         s = "{}({}".format(self.__class__.__name__, TaskRun.state_str(self._state))
@@ -196,7 +374,7 @@ class TaskRun:
             s += ":{}, {}, {}".format(self._host.host_id if isinstance(self._host, Host) else self._host, self._start, self._finish)
         return s + ')'
 
-class Task:
+class Task(object):
     """
     A collection of WorkUnits, which runs on a Host and maintains its
     history of TaskRuns.  A Task has a task ID, for the convenience of
@@ -247,7 +425,7 @@ class Task:
     def run(self, time, host):
         """Starts running this Task on a given Host"""
         if self.state == TaskRun.DONE:
-            raise SimError(time, "Can't rerun a completed task: {}".format(self))
+            raise SimulationError(time, "Can't rerun a completed task: {}".format(self))
         if self._current.finished():
             self._current = TaskRun()
             self._history.append(self._current)
@@ -266,7 +444,7 @@ class Task:
     def __repr__(self):
         return "{}(task_id={}, size={}, scale={}, {})".format(self.__class__.__name__, self._task_id, self.size, self.scale, self._history)
 
-class LinearRuntime:
+class LinearRuntime(object):
     """
     A simple linear model of the running time of a Task, with a slope
     and y-intercept.
@@ -275,14 +453,14 @@ class LinearRuntime:
         self._slope = slope
         self._intercept = intercept
 
-    def runtimeOf(self, task):
+    def runtime_of(self, task):
         """Calculates the running time of a given Task"""
         return self._slope*task.scale + self._intercept
 
     def __repr__(self):
         return "{}({}x + {})".format(self.__class__.__name__, self._slope, self._intercept)
 
-class Host:
+class Host(Actor):
     """
     A model of a Host.  A Host runs Tasks, but has a maximum number of
     Tasks that it can run at once (a capacity).  A Host has intrinsic
@@ -293,6 +471,7 @@ class Host:
     to Task requirements.)
     """
     def __init__(self, host_id, num_slots, runtime_model):
+        super(Host, self).__init__()
         self._host_id = host_id
         if num_slots < 0:
             raise ValueError("Number of Task slots on a Host cannot be negative")
@@ -315,7 +494,7 @@ class Host:
         this Host (for whatever reason), the callback is called.
         """
         if not self.available():
-            raise SimError(time, "Host is full; cannot run Task: {}".format(task))
+            raise SimulationError(time, "Host is full; cannot run Task: {}".format(task))
         self._tasks.append(task)
         self._callback[task] = callback
 
@@ -347,7 +526,7 @@ class Host:
     def __repr__(self):
         return "{}(host_id={}, capacity={}, tasks={})".format(self.__class__.__name__, self.host_id, self.capacity, self._tasks)
 
-class Master:
+class Master(Actor):
     """
     A Master starts with WorkUnits and Hosts.  It creates Tasks from
     the given WorkUnits and submits them to Hosts, trying to process
@@ -355,6 +534,7 @@ class Master:
     returning it to the Master.
     """
     def __init__(self, initial_units, hosts):
+        super(Master, self).__init__()
         self._remaining_units = initial_units[:]
         self._processed_units = []
         if len(hosts) == 0:
@@ -397,10 +577,10 @@ class Master:
         self._graveyard.append(task)
 
     def submit_task(self, time, task):
-        """Selects a Host, and submits a given Task to it."""
+        """Selects a Host, and submits the given Task to it."""
         host = self.select_host(task)
         if host is None:
-            raise SimError(time, "No Host for Task")
+            raise SimulationError(time, "No Host for Task")
         host.run(time, task, lambda t : self.receive_task(t, task))
 
     def receive_task(self, time, task):
